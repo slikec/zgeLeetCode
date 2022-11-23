e272ac34a91e6eabce7f11a90f2b68e55b35b26a
zhenyuge <stglikec@gmail.com>
2022-11-23 22:16:02 +0800

    Leetcode: 1081

---
+++ src/com/zge/leetcode/_1081_SmallestSubsequenceOfDistinctCharacters.java
@@ -0,0 +1,40 @@
+package com.zge.leetcode;
+
+import java.util.Stack;
+
+public class _1081_SmallestSubsequenceOfDistinctCharacters {
+    /*
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     * */
+    public String smallestSubsequence(String s){
+        Stack<Character> stack = new Stack<>();
+        int[] count = new int[256];
+        boolean[] check = new boolean[256];
+
+        for(char c : s.toCharArray()){
+            count[c]++;
+        }
+
+        for(char c : s.toCharArray()){
+            count[c]--;
+
+            if(check[c]) continue;
+
+            while(!stack.isEmpty() && stack.peek() > c){
+                if(count[stack.peek()]==0) break;
+                check[stack.pop()] = false;
+            }
+
+            stack.push(c);
+            check[c] = true;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
